23064f8f6469c4616729a432726636012e9c1234
Bahri Meriç Canlı <bahri@bahri.info>
2026-07-16 10:03:44 +0300

    fix: the_title_attribute(''.''.false) echo bug — esc_attr(get_the_title()) ile değiştir

--- library/structure/content-extensions.php
+++ library/structure/content-extensions.php
@@ -116,7 +116,7 @@ function travelify_theloop_for_archive() {
 				$image = '';
 	     		$title_attribute = apply_filters( 'the_title', get_the_title( $post->ID ) );
 	     		$image .= '<figure class="post-featured-image">';
-	  			$image .= '<a href="' . esc_url( get_permalink() ) . '" title="'.the_title_attribute( '', '', false ).'">';
+	  			$image .= '<a href="' . esc_url( get_permalink() ) . '" title="'.esc_attr( get_the_title() ).'">';
 	  			$image .= get_the_post_thumbnail( $post->ID, 'featured', array( 'title' => esc_attr( $title_attribute ), 'alt' => esc_attr( $title_attribute ) ) ).'</a>';
 	  			$image .= '</figure>';
 
@@ -148,7 +148,7 @@ function travelify_theloop_for_archive() {
 	             	<?php } ?>
     			</div><!-- .entry-meta -->
     			<?php
-    			echo '<a class="readmore" href="' . esc_url( get_permalink() ) . '" title="'.the_title_attribute( '', '', false ).'">'.__( 'Read more', 'travelify' ).'</a>';
+    			echo '<a class="readmore" href="' . esc_url( get_permalink() ) . '" title="'.esc_attr( get_the_title() ).'">'.__( 'Read more', 'travelify' ).'</a>';
     			?>
     		</div>
 
@@ -439,7 +439,7 @@ function travelify_theloop_for_template_blog_image_large() {
 				$image = '';
 	     		$title_attribute = apply_filters( 'the_title', get_the_title( $post->ID ) );
 	     		$image .= '<figure class="post-featured-image">';
-	  			$image .= '<a href="' . esc_url( get_permalink() ) . '" title="'.the_title_attribute( '', '', false ).'">';
+	  			$image .= '<a href="' . esc_url( get_permalink() ) . '" title="'.esc_attr( get_the_title() ).'">';
 	  			$image .= get_the_post_thumbnail( $post->ID, 'featured', array( 'title' => esc_attr( $title_attribute ), 'alt' => esc_attr( $title_attribute ) ) ).'</a>';
 	  			$image .= '</figure>';
 
@@ -470,7 +470,7 @@ function travelify_theloop_for_template_blog_image_large() {
 	             	<?php } ?>
     			</div><!-- .entry-meta -->
     			<?php
-    			echo '<a class="readmore" href="' . esc_url( get_permalink() ) . '" title="'.the_title_attribute( '', '', false ).'">'.__( 'Read more', 'travelify' ).'</a>';
+    			echo '<a class="readmore" href="' . esc_url( get_permalink() ) . '" title="'.esc_attr( get_the_title() ).'">'.__( 'Read more', 'travelify' ).'</a>';
     			?>
     		</div>
 
@@ -555,7 +555,7 @@ function travelify_theloop_for_template_blog_image_medium() {
 				$image = '';
 	     		$title_attribute = apply_filters( 'the_title', get_the_title( $post->ID ) );
 	     		$image .= '<figure class="post-featured-image">';
-	  			$image .= '<a href="' . esc_url( get_permalink() ) . '" title="'.the_title_attribute( '', '', false ).'">';
+	  			$image .= '<a href="' . esc_url( get_permalink() ) . '" title="'.esc_attr( get_the_title() ).'">';
 	  			$image .= get_the_post_thumbnail( $post->ID, 'featured-medium', array( 'title' => esc_attr( $title_attribute ), 'alt' => esc_attr( $title_attribute ) ) ).'</a>';
 	  			$image .= '</figure>';
 
@@ -581,7 +581,7 @@ function travelify_theloop_for_template_blog_image_medium() {
 	             	<?php } ?>
     			</div><!-- .entry-meta -->
     			<?php
-    			echo '<a class="readmore" href="' . esc_url( get_permalink() ) . '" title="'.the_title_attribute( '', '', false ).'">'.__( 'Read more', 'travelify' ).'</a>';
+    			echo '<a class="readmore" href="' . esc_url( get_permalink() ) . '" title="'.esc_attr( get_the_title() ).'">'.__( 'Read more', 'travelify' ).'</a>';
     			?>
     		</div>
 
